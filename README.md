f2eb714375ac0fa2766c7b00418e0e8c07b67128
Anderson Correa <andersonfpcorrea@gmail.com>
2022-09-23 03:13:08 -0300

    docs: update readme

--- README.md
+++ README.md
@@ -1,1 +1,93 @@
-# Todo List
+# _[Todo app](https://master.da3owp9wefdc1.amplifyapp.com/)_
+
+<details>
+<summary><strong>Leia a descrição do projeto em português 🇧🇷</strong></summary>
+<br />
+
+## Descrição
+
+<hr>
+
+Este é um app de lista de afazeres (_todo app_) full-stack:
+
+- Há autenticação de usuário, criação de conta com verificação de email, recuperação de senha e validação de dados no formulário de login.
+- O usuário pode criar tarefas com título e descrição, além de poder fazer upload de imagens.
+- As tarefas ficam salvas no banco de dados da aplicação (e não no local storage).
+
+</br>
+
+## Arquitetura
+
+<hr>
+
+O frontend do projeto foi desenvolvido em React e hospedado com o serviço _[AWS Amplify](https://aws.amazon.com/amplify/)_
+
+A autenticação de usuário foi feita através da _[Amazon Cognito](https://aws.amazon.com/cognito/)_ e de _libraries_ da AWS Amplify.
+
+No backend foi criada uma API _[GraphQL](https://graphql.org/)_ com os serviços _[AWL AppSync](https://aws.amazon.com/pt/appsync/)_ e _[Amazon S3](https://aws.amazon.com/pt/s3/)_ num banco de dados NoSQL (_[Amazon DynamoDB](https://aws.amazon.com/pt/dynamodb/)_).
+
+</br>
+
+## Setup do projeto
+
+<hr>
+
+- React v.18.2.0
+- AWS Amplify CLI v.10.0.0
+
+<br>
+
+## Continuous Delivery (CD)
+
+<hr>
+
+- Processo de entrega contínuo (CD) com _[AWS Amplify](https://master.da3owp9wefdc1.amplifyapp.com/)_.
+
+</details>
+
+<br />
+<br />
+<details open>
+<summary><strong>Read the project's description in English 🇬🇧</strong></summary>
+<br />
+
+## Description
+
+<hr>
+
+This is a full-stack todo app.
+
+- There is user authentication, creation of new accounts with email verification, password recovery and form data validation;
+- The user can create tasks (_todo_ 's) with title and description, and upload an image for each _todo_;
+- All tasks are stored into the app's database (not in the local storage).
+
+</br>
+
+## Architecture
+
+<hr>
+
+The frontend was developed with React and deployed with _[AWS Amplify](https://aws.amazon.com/amplify/)_
+
+User authentication was carried out with AWS Amplify libraries and _[Amazon Cognito](https://aws.amazon.com/cognito/)_.
+
+For the backend it was created a _[GraphQL API](https://graphql.org/)_ with _[AWL AppSync](https://aws.amazon.com/pt/appsync/)_ and _[Amazon S3](https://aws.amazon.com/pt/s3/)_ services, using a NoSQL database (_[Amazon DynamoDB](https://aws.amazon.com/pt/dynamodb/)_).
+
+</br>
+
+## Project Setup
+
+<hr>
+
+- React v.18.2.0
+- AWS Amplify CLI v.10.0.0
+
+<br>
+
+## Continuous Delivery (CD)
+
+<hr>
+
+- Continous delivery (CD) with _[AWS Amplify](https://master.da3owp9wefdc1.amplifyapp.com/)_.
+
+</details>
--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ import {
   deleteTodo as deleteTodoMutation,
 } from './graphql/mutations';
 import { listTodos } from './graphql/queries';
+import Logo from './logo.svg';
 
 const initialFormState = { name: '', description: '' };
 
 function App({ signOut }) {
   const [todos, setTodos] = useState([]);
   const [formData, setFormData] = useState(initialFormState);
+  console.log(todos);
 
   useEffect(() => {
     fetchTodo().catch((err) => console.error(err.message));
@@ -50,6 +52,7 @@ function App({ signOut }) {
     }
     setTodos([...todos, formData]);
     setFormData(initialFormState);
+    document.querySelector('[type="file"]').value = '';
   }
 
   async function deleteTodo({ id }) {
@@ -71,35 +74,58 @@ function App({ signOut }) {
   }
 
   return (
-    <div className='App'>
-      <h1>My Todos App</h1>
-      <input
-        onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-        placeholder='Note name'
-        value={formData.name}
-      />
-      <input
-        onChange={(e) =>
-          setFormData({ ...formData, description: e.target.value })
-        }
-        placeholder='Note description'
-        value={formData.description}
-      />
-      <input type='file' onChange={onChange} />
-      <button onClick={createTodo}>Create Todo</button>
-      <div style={{ marginBottom: 30 }}>
+    <div className='App App-header'>
+      <img src={Logo} alt='react logo' className='App-logo' />
+      <h1 className=''>Full-Stack Todo App</h1>
+      <fieldset className='fieldset'>
+        <input
+          onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+          placeholder='Note name'
+          value={formData.name}
+        />
+        <input
+          onChange={(e) =>
+            setFormData({ ...formData, description: e.target.value })
+          }
+          placeholder='Note description'
+          value={formData.description}
+        />
+        <input type='file' id='file-input' onChange={onChange} />
+        <button onClick={createTodo}>Create Todo</button>
+      </fieldset>
+      <div
+        style={{
+          marginBottom: 30,
+          marginTop: 40,
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '1rem',
+          justifyContent: 'center',
+        }}
+      >
         {todos.map((todo) => (
-          <div key={todo.id || todo.name}>
-            <h2>{todo.name}</h2>
-            <p>{todo.description}</p>
-            {todo.image && (
-              <img src={todo.image} style={{ width: 400 }} alt={todo.name} />
-            )}
-            <button onClick={() => deleteTodo(todo)}>Delete todo</button>
+          <div key={todo.id || todo.name} className='todo-card'>
+            <div style={{ textAlign: 'center' }}>
+              <h2>{todo.name}</h2>
+              <p>{todo.description}</p>
+              <p>
+                Created at {new Date(todo.createdAt).toLocaleDateString()}{' '}
+                {new Date(todo.createdAt).toLocaleTimeString()}
+              </p>
+            </div>
+            <div className='flex-col'>
+              {todo.image && (
+                <img src={todo.image} className='todo-img' alt={todo.name} />
+              )}
+              <button onClick={() => deleteTodo(todo)}>Delete todo</button>
+            </div>
           </div>
         ))}
       </div>
-      <button onClick={signOut}>Sign Out</button>
+      <button onClick={signOut} className='sign-out-btn'>
+        Sign Out
+      </button>
     </div>
   );
 }
