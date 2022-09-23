3289ee8bc3f1ab67d26dc1d4ff516059387e0d00
Anderson Correa <andersonfpcorrea@gmail.com>
2022-09-23 03:26:06 -0300

    refactor: add try-catch blocks on async functions

--- src/App.js
+++ src/App.js
@@ -15,62 +15,77 @@ const initialFormState = { name: '', description: '' };
 function App({ signOut }) {
   const [todos, setTodos] = useState([]);
   const [formData, setFormData] = useState(initialFormState);
-  console.log(todos);
 
   useEffect(() => {
-    fetchTodo().catch((err) => console.error(err.message));
+    fetchTodo();
   }, []);
 
   async function fetchTodo() {
-    const apiData = await API.graphql({
-      query: listTodos,
-      authMode: 'AMAZON_COGNITO_USER_POOLS',
-    });
-    const todosFromAPI = apiData.data.listTodos.items;
-    await Promise.all(
-      todosFromAPI.map(async (note) => {
-        if (note.image) {
-          const image = await Storage.get(note.image);
-          note.image = image;
-        }
-        return note;
-      })
-    );
-    setTodos(apiData.data.listTodos.items);
+    try {
+      const apiData = await API.graphql({
+        query: listTodos,
+        authMode: 'AMAZON_COGNITO_USER_POOLS',
+      });
+      const todosFromAPI = apiData.data.listTodos.items;
+      await Promise.all(
+        todosFromAPI.map(async (note) => {
+          if (note.image) {
+            const image = await Storage.get(note.image);
+            note.image = image;
+          }
+          return note;
+        })
+      );
+      setTodos(apiData.data.listTodos.items);
+    } catch (err) {
+      console.error(`Something was wrong: ${err.message}`);
+    }
   }
 
   async function createTodo() {
-    if (!formData.name || !formData.description) return;
-    await API.graphql({
-      query: createTodoMutation,
-      variables: { input: formData },
-      authMode: 'AMAZON_COGNITO_USER_POOLS',
-    });
-    if (formData.image) {
-      const image = await Storage.get(formData.image);
-      formData.image = image;
+    try {
+      if (!formData.name || !formData.description) return;
+      await API.graphql({
+        query: createTodoMutation,
+        variables: { input: formData },
+        authMode: 'AMAZON_COGNITO_USER_POOLS',
+      });
+      if (formData.image) {
+        const image = await Storage.get(formData.image);
+        formData.image = image;
+      }
+      setTodos([...todos, formData]);
+      setFormData(initialFormState);
+      document.querySelector('[type="file"]').value = '';
+    } catch (err) {
+      console.error(`Something was wrong: ${err.message}`);
     }
-    setTodos([...todos, formData]);
-    setFormData(initialFormState);
-    document.querySelector('[type="file"]').value = '';
   }
 
   async function deleteTodo({ id }) {
-    const newTodosArray = todos.filter((note) => note.id !== id);
-    setTodos(newTodosArray);
-    await API.graphql({
-      query: deleteTodoMutation,
-      variables: { input: { id } },
-      authMode: 'AMAZON_COGNITO_USER_POOLS',
-    });
+    try {
+      const newTodosArray = todos.filter((note) => note.id !== id);
+      setTodos(newTodosArray);
+      await API.graphql({
+        query: deleteTodoMutation,
+        variables: { input: { id } },
+        authMode: 'AMAZON_COGNITO_USER_POOLS',
+      });
+    } catch (err) {
+      console.error(`Something was wrong: ${err.message}`);
+    }
   }
 
   async function onChange(e) {
-    if (!e.target.files[0]) return;
-    const file = e.target.files[0];
-    setFormData({ ...formData, image: file.name });
-    await Storage.put(file.name, file);
-    fetchTodo();
+    try {
+      if (!e.target.files[0]) return;
+      const file = e.target.files[0];
+      setFormData({ ...formData, image: file.name });
+      await Storage.put(file.name, file);
+      fetchTodo();
+    } catch (err) {
+      console.error(`Something was wrong: ${err.message}`);
+    }
   }
 
   return (
